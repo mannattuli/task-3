4728005610f67fa402aec2d1ae43165f84aad2e0
Mannat <mannattuli13@gmail.com>
2022-04-06 22:15:09 +0530

    D

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`app up on port ${port}`);
 });
